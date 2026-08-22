e59073a92ba374c1835d5e6632ae8d4cac7bb797
kunal-shetty <kunalshetty567@gmail.com>
2026-08-22 23:37:07 +0530

    refactor(frontend): remove legacy landing page

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ChatArea } from "@/components/chat-area"
-
-export default function Home() {
-  return (
-    <div className="flex h-screen w-full overflow-hidden bg-background">
-      <ChatArea />
-    </div>
-  )
-}
